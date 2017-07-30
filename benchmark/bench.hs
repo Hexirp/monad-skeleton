88f9b518dceb07cc473fd1e09e7dbe2dc06740d3
Hexirp <Hexirp@users.noreply.github.com>
2017-07-30 21:49:51 +0900

    Add benchmark of Old

--- benchmark/bench.hs
+++ benchmark/bench.hs
@@ -5,6 +5,8 @@ import Control.Applicative
 import Control.Monad.Skeleton
 import Control.Monad.Zombie
 
+import qualified Old as Old
+
 data PF a where
  PF :: PF ()
 
@@ -24,3 +26,23 @@ run x = foldr r [] (disembalm x) where
  r :: MonadView PF (Zombie PF) () -> [Int] -> [Int]
  r (Return ()) xs = 0 : xs
  r (PF :>>= f) xs = (map (+ 1) $ run $ f ()) ++ xs
+
+data OF a where
+ OF :: OF ()
+
+type O a = Old.Zombie OF a
+
+zero_and_one_o :: O ()
+zero_and_one_o = return () <|> Old.liftZ OF
+
+bench_o :: Int -> O ()
+bench_o n = case compare n 0 of
+ LT -> error "negative!"
+ EQ -> return ()
+ GT -> bench_o (n - 1) >> zero_and_one_o
+
+run_o :: O () -> [Int]
+run_o x = foldr r [] (Old.disembalm x) where
+ r :: MonadView OF (Old.Zombie OF) () -> [Int] -> [Int]
+ r (Return ()) xs = 0 : xs
+ r (OF :>>= f) xs = (map (+ 1) $ run_o $ f ()) ++ xs
